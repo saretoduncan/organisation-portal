dc731c74a81aaec9c6ddba09b8577408ed35cc1e
saretoduncan <saretoduncan@gmail.com>
2021-08-21 22:07:56 +0300

    add save and getall method that will save and retrieve all info from db

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,5 +1,11 @@
 package models;
 
+import database_config.DB;
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Department {
     private String name;
     private String description;
@@ -8,6 +14,19 @@ public class Department {
         this.name= name;
         this.description= description;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id);
     }
 
     public String getName() {
@@ -32,5 +51,21 @@ public class Department {
 
     public void setId(int id) {
         this.id = id;
+    }
+    public void  save(){
+        try(Connection connection= DB.sql2o.open()){
+            String sql = "INSERT INTO departments (name, description) VALUES (:name, :description); ";
+            this.id=(int) connection.createQuery(sql,true)
+                    .addParameter("name", this.name)
+                    .addParameter("description", this.description)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Department> getAll(){
+        try(Connection connection= DB.sql2o.open()){
+            String getAll= "SELECT * FROM departments;";
+            return  connection.createQuery(getAll).executeAndFetch(Department.class);
+        }
     }
 }
--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,16 +1,34 @@
 package models;
 
+import database_config.DB;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartmentTest {
     Department testDepartment;
+    @BeforeAll
+    public static void beforeAll(){
+        DB.sql2o= new Sql2o("jdbc:postgresql://localhost:5432/organisation_db_test","softwaredev","1234");
+    }
     @BeforeEach
     public void setUp(){
    testDepartment = new Department("Engineering", "Responsible for defining the configuration of the product and produce the drawing and specification and also responsible to modify the configuration as needed.");
 
+    }
+    @AfterEach
+    public void afterEach(){
+        try (Connection connection= DB.sql2o.open()){
+            String clearDepartments= "DELETE FROM departments *;";
+            connection.createQuery(clearDepartments).executeUpdate();
+
+
+        }
     }
   @Test
     void department_instantiateCorrectly(){
@@ -21,5 +39,11 @@ class DepartmentTest {
         assertEquals("Engineering", testDepartment.getName());
         assertEquals("Responsible for defining the configuration of the product and produce the drawing and specification and also responsible to modify the configuration as needed.",testDepartment.getDescription());
   }
+  @Test
+    void save_InstanceOfDepartments_true(){
+        testDepartment.save();
+        assertEquals(Department.getAll().get(0),testDepartment);
+    }
+
 
 }
