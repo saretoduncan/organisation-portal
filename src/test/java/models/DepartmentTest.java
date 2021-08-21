31e09b91b9f05644ebac83d8c0a6533c5a9dd4a7
saretoduncan <saretoduncan@gmail.com>
2021-08-21 21:16:56 +0300

    add and pass tests for getters and setters for Department test

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,6 +1,25 @@
 package models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DepartmentTest {
-  
+    Department testDepartment;
+    @BeforeEach
+    public void setUp(){
+   testDepartment = new Department("Engineering", "Responsible for defining the configuration of the product and produce the drawing and specification and also responsible to modify the configuration as needed.");
+
+    }
+  @Test
+    void department_instantiateCorrectly(){
+      assertTrue(testDepartment instanceof Department);
+  }
+  @Test
+    void InstanceOfGettersAndSetter_true(){
+        assertEquals("Engineering", testDepartment.getName());
+        assertEquals("Responsible for defining the configuration of the product and produce the drawing and specification and also responsible to modify the configuration as needed.",testDepartment.getDescription());
+  }
+
 }
